8bce4ebdb3f5b761c95471ff3a7b31b41ebc51eb
tuananh <duongtuananh2211@gmail.com>
2019-11-12 11:25:49 +0700

    add button login and register

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +7,7 @@ import {HttpClient} from '@angular/common/http';
 export class AuthService {
 
   apiUrl = 'http://localhost:8000/api/';
+  token;
 
   constructor(private http: HttpClient) {
   }
@@ -17,5 +18,20 @@ export class AuthService {
 
   register(data) {
     return this.http.post(this.apiUrl + 'register', data);
+  }
+
+  logout() {
+    localStorage.removeItem('currentToken');
+  }
+
+  getToken(): string {
+    return localStorage.getItem('currentToken');
+  }
+
+  getUser() {
+    this.token = this.getToken();
+    const headers = new HttpHeaders()
+      .set('Authorization', 'Bearer ' + this.token);
+    return this.http.get(this.apiUrl + 'auth/user', {headers});
   }
 }
