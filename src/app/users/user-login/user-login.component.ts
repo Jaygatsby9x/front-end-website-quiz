dcfbe34668192eaa3cb2d268941a5e212c36d0e2
tuananh <duongtuananh2211@gmail.com>
2019-12-04 08:36:09 +0700

    fixlogin

--- src/app/users/user-login/user-login.component.ts
+++ src/app/users/user-login/user-login.component.ts
@@ -3,6 +3,7 @@ import {Router} from '@angular/router';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {AuthService} from '../../services/auth.service';
 import {IResponse} from '../../interfaces/iresponse';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-user-login',
@@ -14,7 +15,10 @@ export class UserLoginComponent implements OnInit {
   form: FormGroup;
   message;
 
-  constructor(private router: Router, private fb: FormBuilder, private authService: AuthService ) {
+  constructor(private router: Router,
+              private fb: FormBuilder,
+              private authService: AuthService,
+              private toastrService: ToastrService) {
   }
 
   ngOnInit() {
@@ -27,6 +31,7 @@ export class UserLoginComponent implements OnInit {
   login() {
     this.router.navigate(['/']);
   }
+
   onSubmit() {
     const data = this.form.value;
     this.authService.login(data).subscribe((response: IResponse) => {
@@ -34,8 +39,13 @@ export class UserLoginComponent implements OnInit {
       this.authService.setRole(response.role);
       this.router.navigate(['/']);
     }, error => {
+      if (error.status === 401) {
+        this.toastrService.error(
+          'Tài khoản của bạn hiện đang chờ xác nhận,' +
+                  ' vui lòng kiểm tra mail của bạn',
+            'Đăng nhập thất bại!');
+      }
       this.message = error.error.error;
-      console.log(this.message);
     });
   }
 
